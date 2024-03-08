a5a1b87ab5ec7b0e0c81b05edabe4c1495e76f21
mipi1256 <mipi1258@gmail.com>
2024-03-08 17:33:24 +0900

    Select 기초 문법 학습

---
+++ 02_WHERE절.sql
@@ -0,0 +1,53 @@
+
+SELECT * FROM employees;
+
+-- WHERE절 비교 (데이터 값은 대/소문자를 구분합니다.)
+SELECT first_name, last_name, job_id
+FROM employees
+WHERE job_id = 'IT_PROG';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
