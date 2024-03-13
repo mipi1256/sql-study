fac8162f37fcd278dbf36d8bccb1291f82edb0a6
mipi1256 <mipi1258@gmail.com>
2024-03-13 11:37:12 +0900

    연습 문제 수정

--- 09_조인연습문제.sql
+++ 09_조인연습문제.sql
@@ -90,15 +90,18 @@ SELECT * FROM employees, departments;
 급여, 부서명, 근무지를 출력하세요. (Alias를 사용)
 */
 SELECT
-    d.department_id AS 사원번호,
-    e.first_name || ' ' || e.last_name as 이름,
+    e.employee_id AS 사원번호, 
+    e.first_name AS 이름, 
     e.salary AS 급여,
     d.department_name AS 부서명,
-    d.location_id AS 근무지
+    loc.city AS 근무지
 FROM employees e
-INNER JOIN departments d
+JOIN departments d
 ON e.department_id = d.department_id
-WHERE e.job_id = 'SA_MAN';
+JOIN locations loc
+ON d.location_id = loc.location_id
+WHERE e.job_id = 'SA_MAN'
+ORDER BY 사원번호;
 
 
 /*
@@ -111,7 +114,7 @@ SELECT
 FROM employees e
 JOIN jobs j
 ON e.job_id = j.job_id
-WHERE j.job_title = 'Stock Manager' OR j.job_title = 'Stock Clerk';
+WHERE job_title IN ('Stock Manager', 'Stock Clerk');
 
 
 /*
@@ -119,12 +122,11 @@ WHERE j.job_title = 'Stock Manager' OR j.job_title = 'Stock Clerk';
 -- departments 테이블에서 직원이 없는 부서를 찾아 출력하세요. LEFT OUTER JOIN 사용
 */
 SELECT
-    *
-FROM employees e
-LEFT OUTER JOIN departments d
+    d.department_name
+FROM departments d
+LEFT JOIN employees e
 ON d.department_id = e.department_id
-WHERE d.department_name IS NULL;
-
+WHERE e.employee_id IS NULL;
 
 /*
 문제 10. 
@@ -132,8 +134,8 @@ WHERE d.department_name IS NULL;
 힌트) EMPLOYEES 테이블과 EMPLOYEES 테이블을 조인하세요.
 */
 SELECT
-    e1.employee_id, e1.first_name, e1.manager_id,
-    e2.first_name, e2.employee_id
+    e1.first_name,
+    e2.first_name AS manager_name
 FROM employees e1
 JOIN employees e2
 ON e1.manager_id = e2.employee_id;
@@ -145,7 +147,8 @@ ON e1.manager_id = e2.employee_id;
 -- 매니저 아이디가 없는 사람은 배제하고 급여는 역순으로 정렬하세요.
 */
 SELECT
-    e2.first_name, e2.salary
+    e1.employee_id, e1.first_name,e1.manager_id, 
+    e2.first_name AS manager_name, e2.salary
 FROM employees e1
 LEFT JOIN employees e2
 ON e1.manager_id = e2.employee_id
